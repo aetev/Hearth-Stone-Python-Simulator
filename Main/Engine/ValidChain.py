1231cdeb9e34e183a8b98b2f8d778689a34c39bb
aetev <slademcbgc@gmail.com>
2024-09-08 17:10:16 +0000

    Sep 8, 2024, 12:10 PM

--- Main/Engine/ValidChain.py
+++ Main/Engine/ValidChain.py
@@ -1,10 +1,19 @@
-class Handler:
+from abc import ABC, abstractmethod
+
+class Handler(ABC):
     def __init__(self, successor=None):
         self._successor = successor
 
     def handle_request(self, request):
         pass
 
+class ValidationEngine(ABC):
+    def __init__(self):
+        self.handlers = []
+
+    def validate(self, request):
+        for handler in self.handlers:
+            handler.handle_request(request)
 
 class ConcreteHandler1(Handler):
     def handle_request(self, request):
@@ -29,26 +38,25 @@ class ConcreteHandler3(Handler):
         elif self._successor is not None:
             self._successor.handle_request(request)
 
+class ConcreteHandler4(Handler):
+    def handle_request(self, request):
+        if request == "request3":
+            print("ConcreteHandler3 handles the request.")
+        elif self._successor is not None:
+            self._successor.handle_request(request)
 
-class ValidationEngine:
+
+class handValidator(ValidationEngine):
     def __init__(self):
-        handlers = (
+        self.handlers = [
             ConcreteHandler1(),
             ConcreteHandler2(),
             ConcreteHandler3(),
-        )
-
-        for i in range(len(handlers) - 1):
-            handlers[i]._successor = handlers[i+1]
-
-        self._handler = handlers[0]
-
-    def validate(self, request):
-        self._handler.handle_request(request)
-
+            ConcreteHandler4()
+        ]
 
 # Usage example
-engine = ValidationEngine()
+engine = handValidator()
 engine.validate("request1")
 engine.validate("request2")
 engine.validate("request3")
