64ade41c3b1f726f64ae87670e2bc609d731d0d8
aetev <slademcbgc@gmail.com>
2024-09-09 16:48:37 +0000

    Sep 9, 2024, 11:48 AM

--- Main/Entity/Player.py
+++ Main/Entity/Player.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from CardLib.CardBase import Card
 
 
 @dataclass
 class Player:
-    health = 30
-    deck = None
-    hand = None
+    board: list[Card] = None
+    deck: list[Card] = None
+    hand: list[Card] = None
+    max_health: int = 30
+    health: int = 30
