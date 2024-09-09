f8fc1a0c7d37a5c7dc1c00ce5c866e1db26c50e7
aetev <slademcbgc@gmail.com>
2024-09-09 16:37:35 +0000

    Sep 9, 2024, 11:37 AM

--- Main/Entity/Board.py
+++ Main/Entity/Board.py
@@ -2,16 +2,8 @@ from .Player import Player
 from dataclasses import dataclass
 
 
-
 @dataclass
 class Board:
-    players: list[Player]
-    current_player: int
-
-    def __init__(self):
-        self.players = [Player(), Player()]
-        self.current_player = 0
-
-    def end_turn(self):
-        self.current_player = 1 - self.current_player
-        self.event_manager.trigger_event("turn_end", self.current_player)
+    player_1: Player
+    player_2: Player
+    active_player: int
